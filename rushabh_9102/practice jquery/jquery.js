9a2a356bd83e1417bd1754844157a5291319b5f1
Rushabhgadhiya9102 <rushabhgadhiya9102@gmail.com>
2024-05-23 00:03:06 +0530

    task-completed

--- rushabh_9102/practice jquery/jquery.js
+++ rushabh_9102/practice jquery/jquery.js
@@ -1,4 +1,13 @@
+
+// -------- L I G H T - D A R K - M O D E - T H E M E ------------
+
 $("#toggle-theme").on("click",function(){
    $("body").toggleClass("light-mode");
    $("body").toggleClass("dark-mode");
 })
+
+// -------L O G I N - F O R M ---------
+
+$('.fa-right-to-bracket').on('click',function(){
+   $('.main-form-banner').toggle(200);
+});
