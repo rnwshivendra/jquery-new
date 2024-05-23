494bee080c3c2a3abcc9f6a00d737ee6dc14f90e
akshar66 <aksharparekh401@gmail.com>
2024-05-23 22:21:46 +0530

    complete

---
+++ akshar66/Jquery/L3/jquery.js
@@ -0,0 +1,32 @@
+
+$(".bt1").on("click",function(){
+
+    $(".main").toggleClass("night");
+
+})
+
+
+
+
+$(".bt2").on("click",function(){
+
+    $(".box2").toggleClass("flex");
+
+})
+
+
+
+$(".bt3").on("click",function(){
+
+    $(".box3").slideToggle();
+
+})
+
+
+
+
+$(".bt4").on("click",function(){
+
+    $(".box4").fadeToggle(1000);
+
+})
