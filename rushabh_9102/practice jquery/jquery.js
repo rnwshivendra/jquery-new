174716ee65295bd0d5a5fa7abdac19106db0f27c
Rushabhgadhiya9102 <rushabhgadhiya9102@gmail.com>
2024-05-23 18:19:00 +0530

    commited

--- rushabh_9102/practice jquery/jquery.js
+++ rushabh_9102/practice jquery/jquery.js
@@ -1,13 +1,52 @@
-
 // -------- L I G H T - D A R K - M O D E - T H E M E ------------
 
-$("#toggle-theme").on("click",function(){
-   $("body").toggleClass("light-mode");
-   $("body").toggleClass("dark-mode");
-})
+// $(".fa-sun").on("click", function () {
+//   $("body").toggleClass("dark-mode");
+// });
 
 // -------L O G I N - F O R M ---------
 
-$('.fa-right-to-bracket').on('click',function(){
-   $('.main-form-banner').toggle(200);
+$(document).ready(function () {
+  $(".fa-xmark").hide();
+
+  // ------- E X P A N D ---------
+
+  $(".fa-right-to-bracket").on("click", function () {
+    $(".main-form-banner").slideDown("slow");
+    $(".fa-right-to-bracket").hide();
+    $(".fa-xmark").show();
+  });
+
+  // ------- C O L L A P S E ---------
+
+  $(".fa-xmark").on("click", function () {
+    $(".main-form-banner").slideUp("slow");
+    $(".fa-xmark").hide();
+    $(".fa-right-to-bracket").show();
+  });
+});
+// -------L O G I N - F O R M - E N D ---------
+
+
+
+// -------- L I G H T - D A R K - M O D E - T H E M E ------------
+
+$(document).ready(function () {
+  $(".fa-moon").hide();
+
+  // ------- light-mode ---------
+
+  $(".fa-sun").on("click", function () {
+    $("body").toggleClass("dark-mode");
+    $(".fa-sun").hide();
+    $(".fa-moon").show();
+  });
+
+  // ------- dark-mode ---------
+
+  $(".fa-moon").on("click", function () {
+    $("body").toggleClass("light-mode");
+    $(".fa-moon").hide();
+    $(".fa-sun").show();
+  });
 });
