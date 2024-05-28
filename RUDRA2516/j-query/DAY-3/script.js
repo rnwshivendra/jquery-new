2c7172dfcfaa43eb7050cd328f912b310d456af5
rudragondaliya <gondaliyarudra61@gmail.com>
2024-05-28 18:24:13 +0530

    particle

--- RUDRA2516/j-query/DAY-3/script.js
+++ RUDRA2516/j-query/DAY-3/script.js
@@ -1,13 +1,22 @@
 $(document).ready(function(){
     $('#toggle').on("click",function(){
         if(this.checked) {
+            $("h3").css("color","white")
+            $("h1").css("color","white")
+            $("p").css("color","white")
+            $("#login").css("color","black")
             $('body').addClass('black');
-        } else {
-            $('body').removeClass('white');
+        }         
+            else {
+                $('body').removeClass('black');
+                $("h3").css("color","black")
+                $("h1").css("color","black")
+                $("p").css("color","black")
+            }
         
-        }
-    });
-});
+        })
+    })
+
 
 
 $('.slider').slick();
@@ -19,11 +28,21 @@ $(document).ready(function(){
     
         $(".container").slideToggle("1000",function(){
             
+            $(".form-container").css("backdrop-filter","blur","120px"); 
         })
 
     })
 })
 
+
+
+$('.men-wear').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3
+  });
+                  
+  
 
 
 
